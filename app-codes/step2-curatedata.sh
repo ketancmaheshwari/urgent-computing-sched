294c5a74ef48e8128a81f15d603707a5ddcafbd9
Ketan Maheshwari <km0@ornl.gov>
2025-06-23 16:11:43 -0400

    updating the data curation script

--- app-codes/step2-curatedata.sh
+++ app-codes/step2-curatedata.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 DATALOC=$1
+PARENTDIR=$(dirname "$DATALOC")
+mkdir -p "$PARENTDIR"/csvdata
 
-#mkdir -p $DATALOC/csvdata
-#
-#TMPFILE=$(mktemp /tmp/XXXXX)
-#
-#for eachfile in $(find $DATALOC -iname '*.txt')
-#do
-#  tr ',' ';' < "$eachfile" > "$TMPFILE"
-#  tr '|' ',' < "$TMPFILE" > "$DATALOC"/csvdata/"$eachfile".csv && rm "$TMPFILE"
-#  awk -F, 'BEGIN{OFS=","}NF==60{print $0}' "$DATALOC"/csvdata/"$eachfile".csv > tmp && mv tmp "$DATALOC"/csvdata/"$eachfile".csv
-#done
+TMPFILE=$(mktemp /tmp/XXXXX)
 
-echo "$DATALOC/csvdata"
+for eachfile in $(find $DATALOC -iname '*.txt')
+do
+  fname=$(basename $eachfile)
+  tr ',' ';' < "$eachfile" > "$TMPFILE"
+  tr '|' ',' < "$TMPFILE" > "$PARENTDIR"/csvdata/"$fname".csv && rm "$TMPFILE"
+  awk -F, 'BEGIN{OFS=","}NF==60{print $0}' "$PARENTDIR"/csvdata/"$fname".csv > "$TMPFILE" && mv "$TMPFILE" "$PARENTDIR"/csvdata/"$fname".csv
+done
+
+echo -n "$PARENTDIR/csvdata"
